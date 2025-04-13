b7fab1ea3979d9308e44a9320a87294b82bd54af
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-04-13 15:55:30 -0300

    Feat: PoS-Service > Negocio > Implementation, Interfaces

---
+++ PoS-Service/Implementation/NegocioService.cs
@@ -0,0 +1,25 @@
+﻿
+using PoS_Repository.Entities;
+using PoS_Repository.Interfaces;
+using PoS_Service.Interfaces;
+
+namespace PoS_Service.Implementation
+{
+    class NegocioService : INegocioService
+    {
+        private readonly INegocioRepository _negocioRepository;
+        public NegocioService(INegocioRepository negocioRepository)
+        {
+            _negocioRepository = negocioRepository;
+        }
+        public async Task<Negocio> Obtener()
+        {
+            return await _negocioRepository.Obtener();
+        }
+
+        public async Task Editar(Negocio objeto)
+        {
+            await _negocioRepository.Editar(objeto);
+        }
+    }
+}
---
+++ PoS-Service/Interfaces/INegocioService.cs
@@ -0,0 +1,11 @@
+﻿
+using PoS_Repository.Entities;
+
+namespace PoS_Service.Interfaces
+{
+    public interface INegocioService
+    {
+        Task<Negocio> Obtener();
+        Task Editar(Negocio objeto);
+    }
+}
