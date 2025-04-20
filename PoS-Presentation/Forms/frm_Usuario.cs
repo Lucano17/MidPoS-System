aa8180ac1ba146511f4073d543b4098464d1f87e
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-04-20 18:22:06 -0300

    Feat: PoS-Service > Correo > MailKit & MimeKit

---
+++ PoS-Presentation/Forms/frm_Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PoS_Presentation.Forms
+{
+    public partial class frm_Usuario: Form
+    {
+        public frm_Usuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PoS-Service/Interfaces/ICorreoService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PoS_Service.Interfaces
+{
+    public interface ICorreoService
+    {
+        Task Enviar(string para, string asunto, string mensajeHtml);
+    }
+}
